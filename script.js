4eac75ef4f7c5bf0901f0f285960449f85aea668
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-28 15:52:40 +0100

    added search functionality

--- script.js
+++ script.js
@@ -14,23 +14,26 @@ const addToCartBtn = () => {
     )
 }
 
-fetch('https://striveschool-api.herokuapp.com/books')
-.then(response => response.json())
-.then(data => booksContainer.innerHTML = data.map(book => `
-<div class="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
-    <div class="card">
-        <img src=${book.img} class="card-img-top book-image" alt="...">
-        <div class="card-body">
-            <p class="card-text font-weight-bold">${book.title}</p>
-            <p class="card-text">${book.price}</p>
-            <button class="btn btn-success addToCart">Add To cart</button>
+const fetchBooks = () => {
+    fetch('https://striveschool-api.herokuapp.com/books')
+    .then(response => response.json())
+    .then(data => booksContainer.innerHTML = data.map(book => `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
+        <div class="card">
+            <img src=${book.img} class="card-img-top book-image" alt="...">
+            <div class="card-body">
+                <p class="card-text font-weight-bold">${book.title}</p>
+                <p class="card-text">${book.price}</p>
+                <button class="btn btn-success addToCart">Add To cart</button>
+            </div>
         </div>
     </div>
-</div>
-`).join(''))
-.then(addToCartBtn)
-.catch(err => err)
+    `).join(''))
+    .then(addToCartBtn)
+    .catch(err => err)
+}
 
+fetchBooks()
 
 const addToCart = e => {
     const btn = e.target
@@ -52,17 +55,50 @@ const updateCartNavbarDisplay = () => {
 }
     
 const updateCartSection = () => {
+    const cartHeading = document.querySelector('.cart-heading')
     if (shoppingCart.quantity === 0) {
-        cartContainer.innerHTML = `
-        <div class="col-12 text-center">
-            <h3>Your Cart Is Empty</h3>
-        </div>`
+        cartHeading.innerText = 'Your Cart Is Empty'
     } else {
+        cartHeading.innerText = `Your Cart Has ${shoppingCart.quantity} Item`
         cartContainer.innerHTML = shoppingCart.items.map(item => `
         <div class="col-12 d-flex justify-content-between">
             <p>${item.title}</p>
             <p>${item.price}</p>
         </div>
         `)
+    }
+}
+
+const searchInput = document.querySelector('#searchInput')
+const searchBtn = document.querySelector('#searchBtn')
+
+searchInput.addEventListener('keyup', () => {
+    searchBooks()
+})
+
+const searchBooks = () => {
+    if (searchInput.value.length <= 2) {
+        fetchBooks()
+    }
+    if (searchInput.value.length > 2) {
+        fetch('https://striveschool-api.herokuapp.com/books')
+        .then(response => response.json())
+        .then(data => {
+            const filtereBooks = data.filter(book => book.title.includes(searchInput.value))
+            booksContainer.innerHTML = filtereBooks.map(book => `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 py-2">
+                <div class="card">
+                    <img src=${book.img} class="card-img-top book-image" alt="...">
+                    <div class="card-body">
+                        <p class="card-text font-weight-bold">${book.title}</p>
+                        <p class="card-text">${book.price}</p>
+                        <button class="btn btn-success addToCart">Add To cart</button>
+                    </div>
+                </div>
+            </div>
+        `).join('')
+        })
+        .then(addToCartBtn)
+        .catch(err => err)
     }
 }
